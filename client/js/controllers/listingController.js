c9a09fce7bc945ea8f2599f5c663ff013ddd1868
Matthew Barta <matthewbartaa@gmail.com>
2018-10-17 18:18:51 -0400

    Added search bar with search function

--- client/js/controllers/listingController.js
+++ client/js/controllers/listingController.js
@@ -1,19 +1,19 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
-  function($scope, Listings) {
-    /* Get all the listings, then bind it to the scope */
-    Listings.getAll().then(function(response) {
-      $scope.listings = response.data;
-    }, function(error) {
-      console.log('Unable to retrieve listings:', error);
-    });
-
-    $scope.detailedInfo = undefined;
-
-    $scope.addListing = function() {
-
-      Listings.create($scope.newListing).then(function(response) {}, function(error) {
-        console.log('Unable to add listing:', error);
+  angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+    function($scope, Listings) {
+      /* Get all the listings, then bind it to the scope */
+      Listings.getAll().then(function(response) {
+        $scope.listings = response.data;
+      }, function(error) {
+        console.log('Unable to retrieve listings:', error);
       });
+
+      $scope.detailedInfo = undefined;
+
+      $scope.addListing = function() {
+
+        Listings.create($scope.newListing).then(function(response) {}, function(error) {
+          console.log('Unable to add listing:', error);
+        });
 
       //Refresh the page
       location.reload();
@@ -26,10 +26,25 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       });
       //Refresh the page upon deletion.
       location.reload();
-  };
+    };
 
     $scope.showDetails = function(index) {
       $scope.detailedInfo = $scope.listings[index];
     };
-  }
-]);
+
+    $scope.searchFilterCustom = function(entry) {
+     
+      return function(entry) {
+        if(typeof $scope.filterText == 'undefined')
+        {
+          return entry;
+        }
+        
+        if(entry.name.includes($scope.filterText) || entry.code.includes($scope.filterText))
+        {
+         return entry;
+       }
+     }
+   };
+ }
+ ]);
