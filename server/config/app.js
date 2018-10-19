0ff8639a1ada81e88e09247014fc7e99a1abe8db
williamsnoah17 <williamsnoah17@ufl.edu>
2018-10-18 21:45:44 -0400

    Add files via upload

--- server/config/app.js
+++ server/config/app.js
@@ -1,10 +1,11 @@
-var config = require('./config'), 
-    mongoose = require('mongoose'),   
+var config = require('./config'),
+    mongoose = require('mongoose'),
     express = require('./express');
 
 module.exports.start = function() {
-  var app = express.init();
-  app.listen(config.port, function() {
-    console.log('App listening on port', config.port);
-  });
+    var app = express.init();
+    app.listen((process.env.PORT || config.port), function() {
+        console.log('App listening on port', config.port);
+    });
 };
+
