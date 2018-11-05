9d9f2c431b3417dfd19f79c943d37ef38479b38e
Matthew Barta <matthewbartaa@gmail.com>
2018-11-05 11:55:13 -0500

    creating users works .. for now

--- client/login.js
+++ client/login.js
@@ -3,6 +3,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     // User is signed in.
     document.getElementById("in_div").style.display = "none";
     document.getElementById("out_div").style.display = "initial";
+
 } else {
     // No user is signed in.
     document.getElementById("in_div").style.display = "initial";
@@ -15,13 +16,42 @@ function login() {
 	var email = document.getElementById("email_field").value;
 	var password = document.getElementById("password_field").value;
 
-	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+	firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+
+	}).catch(function(error) {
   	// Handle Errors here.
   	var errorCode = error.code;
   	var errorMessage = error.message;
 
   	window.alert("Error : " + errorMessage);
+
   // ...
-});
+	});
 
 }
+
+function logout() {
+	firebase.auth().signOut().then(function() {
+  		// Sign-out successful.
+  		document.location.href = "index.html";
+  	}).catch(function(error) {
+  		// An error happened.
+  		var errorMessage = error.message;
+  		window.alert("Error : " + errorMessage);
+  	});
+
+  }
+
+  function createAccount() {
+
+  	var email = document.getElementById("new_email_field").value;
+  	var password = document.getElementById("new_password_field").value;
+
+  	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  		// Handle Errors here.
+  	var errorCode = error.code;
+  	var errorMessage = error.message;
+ 		 // ...
+ 	 window.alert("Error : " + errorMessage);
+	});
+  }
