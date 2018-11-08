893583ef2f9325870066545fba221cbe157f61ca
williamsnoah17 <williamsnoah17@ufl.edu>
2018-11-08 09:48:38 -0500

    Add files via upload

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,15 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('http://localhost:8080/api/listings');
+        return $http.get('https://nullclass.herokuapp.com/api/listings');
     },
 	
-	create: function(listing) {
-	  return $http.post('http://localhost:8080/api/listings', listing);
+    create: function (listing) {
+        return $http.post('https://nullclass.herokuapp.com/api/listings', listing);
     }, 
 
     delete: function(id) {
-    return $http.delete(`http://localhost:8080/api/listings/${id}`);
+	   /**TODO
+        return result of HTTP delete method
+       */
+        return $http.delete('https://nullclass.herokuapp.com/api/listings/' + id);
     }
   };
 
